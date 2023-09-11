f21f062cd05424aee558aa4d92ebbc7a6fdc8edb
priya <arul.chera@gmail.com>
2023-09-11 17:58:58 +0530

    first cmt Fibonacci series

---
+++ basic calc.py
@@ -0,0 +1,20 @@
+a=int(input("enter the first num:"))
+b=int(input("enter the second num:"))
+c=a+b
+print("the sum is:",c)
+d=a*b
+print("the multiplication is:",d)
+""" shellscripting 
+#!/bin/bash
+n3=0
+read -p "enter n1" n1
+read -p "enter n2" n2
+echo "n1:" $n1
+echo "n2:" $n2
+n3=$(($n1 + $n2))
+echo "sum is " $n3
+n4=$(($n1 * $n2))
+echo "product is" $n4
+"""
+
+
